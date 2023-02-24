da8f668391c827fbf5196ab74134fe32ed4b7f7b
willy3k <willy3k1446d@gmail.com>
2023-02-24 09:59:41 -0300

    luluzinhas

--- src/pages/SobreLuluzinhas/index.js
+++ src/pages/SobreLuluzinhas/index.js
@@ -1,51 +1,45 @@
 import React from 'react';
 import { Container } from '../../style/GlobalStyles';
 import { SecSobrePerolas, SecInicio, Footerr } from './styled';
-import sobreC from '../../assets/img/sobreC.jpeg';
-import EBD from '../../assets/img/diaEBD.jpeg';
-import ebdNaPratica from '../../assets/img/ebdNaPratica.jpeg';
-import ensinando from '../../assets/img/ensinando.jpeg';
-import diaPascoa from '../../assets/img/diaPascoa.jpeg';
+import luluzinhasC from '../../assets/img/luluzinhasC.jpg';
+import luluzinhasI from '../../assets/img/luluzinhasI.jpeg';
+import luluzinhasD from '../../assets/img/luluzinhasD.jpeg';
+import luluzinhasZ from '../../assets/img/luluzinhasZ.jpeg';
+import luluzinhas from '../../assets/img/luluzinhas.jpg';
 
 export default function SobreLuluzinhas() {
   return (
     <Container>
       <SecInicio>
         <div className="fundo">
-          <img src={sobreC} alt="" />
+          <img src={luluzinhasC} alt="" />
           <div className="title">
-            <span>EBD da Crianças</span>
-            <h1>
-              Nossa <samp>EBD</samp> das Crianças
-            </h1>
+            <span>LULUZINHAS</span>
+            <h1>Nosso encontro de jovens Mulheres</h1>
           </div>
         </div>
       </SecInicio>
       <SecSobrePerolas>
-        <h2>Ministério de Crianças</h2>
+        <h2>Ministério das Luluzinhas</h2>
         <div className="card">
           <p>
             Ministério com crianças chamado de ebd. <br />
-            Durante o sábado pela manhã, as crianças participam de uma aula. O
-            objetivo dessa aula é ensinar a Bíblia em uma linguagem que as
-            crianças tenham maior facilidade de entender, mostrando o amor de
-            Deus e os ensinamentos de Sua Palavra.
+            Todas as sextas ás 6h, as meninas participam de um encontro. O
+            objetivo desse encontro é ensinar a Bíblia, mostrando o amor de Deus
+            e os ensinamentos de Sua Palavra.
           </p>
         </div>
         <div className="card segundo">
           <div className="cards">
             <div className="imge">
-              <img src={EBD} alt="" />
+              <img src={luluzinhas} alt="" />
             </div>
             <div className="desc">
-              <h2>Escola Biblica</h2>
+              <h2>Congregação de Jovens Mulheres</h2>
               <p>
-                Escola Biblica das Nossas crianças, todos os sabádos, tem essa
-                aula, que ensina da maneira simples e pratica, que elas venha a
-                entender e aprender cada vez mais que Deus é o salvador, que ele
-                é o omega e alfa(começo e o fim), e que todas as coisas foram
-                feitas por ele, e que desse forma elas possa aprender e logo
-                conseguir suas familias para cristo.
+                Momento Depois de um congresso de joven, maravilho e muito
+                edificante, Nesse congresso, aprendemos a ser como cristo, e o
+                que devemos fazer a parti disso, Eu sou a Luz do Mundo e você?
               </p>
             </div>
           </div>
@@ -54,17 +48,15 @@ export default function SobreLuluzinhas() {
         <div className="card">
           <div className="cards">
             <div className="desc">
-              <h2>EBD Na pratica</h2>
+              <h2>Paceio piquiniquer das Luluzinhas</h2>
               <p>
-                Aprendendo Na pratica, atraves de ensenaçoes de passagem
-                biblicas, para que assim eles possam aprendem atraves deles
-                mesmos, usando a imaginaçoes deles, e assim poderem aprender
-                cada vez mais de Cristo, e sem contar que e de uma forma
-                divertida, e simples.
+                Encontro diferente, Elas se reúniram, e marcaram de fazer um dia
+                deferente, las elas não só se divertiram como também aprenderam,
+                de Cristo.
               </p>
             </div>
             <div className="imge">
-              <img src={ebdNaPratica} alt="" />
+              <img src={luluzinhasD} alt="" />
             </div>
           </div>
         </div>
@@ -72,16 +64,15 @@ export default function SobreLuluzinhas() {
         <div className="card segundo">
           <div className="cards">
             <div className="imge">
-              <img src={ensinando} alt="" />
+              <img src={luluzinhasZ} alt="" />
             </div>
             <div className="desc">
-              <h2>Aprendendo Os Livros da Biblias</h2>
+              <h2>Encontro das Luluzinhas na casa do Senhor</h2>
               <p>
-                Aprendendo os Livros da bíblia, De forma pratica, para o
-                aprendizado tecnicos deles desde do inico, para que no futuro
-                não tenha dificuldades nesse aspecto, sem conta que amplia os
-                conhecimentos deles, e assim dispertando o interesse de querer
-                saber o que esta escrito em cada livro e o que acontece neles.
+                Encontro na casa de Senhor, compartilhando e aprendendo cada vez
+                mais de Deus, e ensinando por qual caminho elas devem andar,
+                juntas elas aprendem e ensinam umas as outras, com testemunhos e
+                esperiencas de vida.
               </p>
             </div>
           </div>
@@ -90,14 +81,18 @@ export default function SobreLuluzinhas() {
         <div className="card">
           <div className="cards">
             <div className="desc">
-              <h2>Ganhando Ovo da Pascoa</h2>
+              <h2>Encontro abençoado na Casa do Senhor</h2>
               <p>
-                Dia de Presente, além de muita estudo, aprendizado e diverção,
-                Presente, ove da pascoa.
+                Esse Ministério assim como os outro, tem um papel muito
+                importante pois é nesses momentos que elas se abrem pedem ajuda,
+                pede conseilhos e assim ganhando a confiança umas das outras,
+                para que assim possam crescer espiritualmente, e também Como
+                mulheres de Deus, para que quando cheguer as adivercidades, elas
+                saibem lhe da com elas, e assim permanesserem em cristo Jesus.
               </p>
             </div>
             <div className="imge">
-              <img src={diaPascoa} alt="" />
+              <img src={luluzinhasI} alt="" />
             </div>
           </div>
         </div>
