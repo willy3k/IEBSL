537dbd82e7979e2688e4b19ce19821327631aab5
willy3k <willy3k1446d@gmail.com>
2023-02-24 17:36:28 -0300

    styled Bolinhas

--- src/pages/PaDaProgramacao/index.js
+++ src/pages/PaDaProgramacao/index.js
@@ -1,14 +1,115 @@
 import React from 'react';
 import { Container } from '../../style/GlobalStyles';
+// import fundo from '../../assets/img/fundo3.jpeg';
 
-import { SecSobreNos, Footerr } from './styled';
+import { SecSobreNos, SecProgramaçao, Footerr } from './styled';
 
 export default function PaDaProgramacao() {
   return (
     <Container>
       <SecSobreNos>
-        <h2>Sobre-Nós</h2>
+        <div className="fundo">
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+          <div className="fundo2">
+            <div className="stars" />
+          </div>
+
+          {/* <img src={fundo} alt="" /> */}
+          <div className="title">
+            <span>PROGRAMAÇÃO</span>
+            <h1>Nossa Programação</h1>
+          </div>
+        </div>
       </SecSobreNos>
+      {/*  */}
+      <SecProgramaçao>
+        <h2>Ola</h2>
+        <div className="card">
+          <div className="dia">
+            <h2>Domingo</h2>
+            <button type="submit">
+              <samp>+</samp>
+            </button>
+          </div>
+          <div className="eventos">
+            <h3>
+              <span>08:00</span> EBD Escola Biblica Dominical
+            </h3>
+          </div>
+        </div>
+        <div className="card">
+          {' '}
+          <div className="dia">
+            <h2>Quarta</h2>
+            <button type="submit">
+              <samp>+</samp>
+            </button>
+          </div>
+          <div className="eventos">
+            <h3>
+              <span>06:00</span> Culto de Oração
+            </h3>
+          </div>
+        </div>
+        <div className="card">
+          {' '}
+          <div className="dia">
+            <h2>Sexta-feira</h2>
+            <button type="submit">
+              <samp>+</samp>
+            </button>
+          </div>
+          <div className="eventos">
+            <h3>
+              <span>06:00</span> Encontro Das Luluzinhas
+            </h3>
+            <h3>
+              <span>06:30</span> Encontro Dos Bolinhas
+            </h3>
+          </div>
+        </div>
+        <div className="card">
+          {' '}
+          <div className="dia">
+            <h2>Sabádo</h2>
+            <button type="submit">
+              <samp>+</samp>
+            </button>
+          </div>
+          <div className="eventos">
+            <h3>
+              <span>10:00</span> EBD Escola Biblica Dominical Das Crianças
+            </h3>
+            <h3>
+              <span>06:30</span> Culto de Adoração
+            </h3>
+          </div>
+        </div>
+      </SecProgramaçao>
+      {/*  */}
       <Footerr>
         <footer>
           <h4>@ Igreja Batista Evangelica Em São Lazaro </h4>
--- src/pages/PaDaProgramacao/styled.js
+++ src/pages/PaDaProgramacao/styled.js
@@ -2,7 +2,156 @@ import styled from 'styled-components';
 import * as colors from '../../config/colors';
 
 export const SecSobreNos = styled.section`
-  background: white;
+  width: 100%;
+  /* background: black; */
+
+  .fundo {
+    display: flex;
+    position: relative;
+    width: 100%;
+    color: white;
+  }
+  .fundo2 {
+    background: linear-gradient(0, #120c56, #000000);
+    width: 100%;
+    height: 450px;
+  }
+  .stars {
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    box-shadow: 50px 30px white, 100px 80px white, 80px 120px white,
+      300px 20px white, 250px 130px white, 200px 50px white, 150px 100px white,
+      320px 100px white;
+    animation: anim-stars 10s linear infinite;
+    transition: 1s;
+  }
+
+  @keyframes anim-stars {
+    from {
+      transform: translateY(0px);
+    }
+    to {
+      transform: translateY(350px);
+    }
+  }
+  img {
+    background: black;
+    opacity: 0.2;
+    width: 100%;
+    height: 450px;
+  }
+  .title {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: absolute;
+    margin-top: 10rem;
+    transition: 1s;
+  }
+  span {
+    font-size: 20px;
+    margin-bottom: 5px;
+  }
+  h1 {
+    text-align: center;
+    font-size: 50px;
+  }
+
+  @media (max-width: 900px) {
+    width: 100%;
+    background: black;
+    height: 250px;
+
+    .fundo2 {
+      background: linear-gradient(0, #120c56, #000000);
+      width: 100%;
+      height: 250px;
+    }
+    @keyframes anim-stars {
+      from {
+        transform: translateY(0px);
+      }
+      to {
+        transform: translateY(150px);
+      }
+    }
+    img {
+      background: black;
+      opacity: 0.3;
+      width: 100%;
+      height: 250px;
+    }
+    .title {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      position: absolute;
+      margin-top: 5rem;
+    }
+    h1 {
+      text-align: center;
+      font-size: 40px;
+    }
+  }
+`;
+
+export const SecProgramaçao = styled.section`
+  color: white;
+  border: 1px solid;
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px 0;
+    background: ${colors.primaryDarkColor};
+    width: 85%;
+    border-radius: 8px;
+  }
+  samp {
+    font-size: 25px;
+    padding: 0px;
+  }
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    background: ${colors.primaryHeader};
+    border-radius: 50%;
+    &:hover {
+      background: black;
+    }
+  }
+
+  .dia {
+    padding: 20px 30px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+  .eventos {
+    padding: 20px 30px;
+    width: 100%;
+    margin-bottom: 20px;
+  }
+  h3 {
+    border-left: 3px solid ${colors.primaryHeader};
+    padding-left: 5px;
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
 `;
 
 export const Footerr = styled.footer`
