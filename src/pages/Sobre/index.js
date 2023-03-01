1a967d49d958844426c92fd8d21572e9956965dc
willy3k <willy3k1446d@gmail.com>
2023-03-01 13:57:49 -0300

    sobre a historia da nossa igreja

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -13,15 +13,21 @@ export default function Ministerio() {
           <h3>Quem Nós somos</h3>
           <h2>Igreja Evangelica Batista Em São Lazaro</h2>
           <p>
-            Fundada em 7 de dezembro 1942, a Igreja Batista do Fragata tem sido
-            instrumento de proclamação do evangelho e edificação dos cristãos na
-            cidade de Pelotas.
+            HÁ 17 ANOS ATRÁS, A EQUIPE DE MISSÕES MINISTÉRIO RASE RESOLVEU
+            VOLTAR AO VILAREJO DE SÃO LÁZARO 8 ANOS APÓS O PRIMEIRO CONTATO E A
+            FORMAÇÃO DE UMA CONGREGAÇÃO QUE FICOU NA RESPONSABILIDADE DA IGREJA
+            DE CUPIRA QUE NÃO DEU CONTINUIDADE AO TRABALHO E O MINBISTÉIO PAROU,
+            RETORNANDO COM O MINISTÉRIO RASE UMA VEZ POR MÊS FAZENDO VISITAS,
+            CULTO, ATIVIDADES COM CRIANÇAS E AÇÃO SOCIAL. INICIAMOS UMA PARCERIA
+            COM A ONG MINHA ESPERANÇA O QUAL SE ENCERROU EM 2012, O QUAL DEU-SE
+            O INÍCIO DA IGREJA COM A FREQUENCIA E ORGANIZAÇÃO DAS ATIVIDADES
+            COMO MINISTERIO INFANTIL, LOUVOR, COREOGRAFIA, TEATRO, DIACONATO, OS
+            GRUPOS PEQUENOS E ORGANIZAÇÕES. APÓS 3 ANOS, O ENTÃO MISSIONÁRIO,
+            FOI ACLAMADO PASTOR DE NOSSA IGREJA, MUDANDO ASSIM DE CONGREGAÇÃO
+            EVANGÉLICA BATISTA EM SÃO LÁZARO PARA IGREJA EVANGÉLICA BATISTA EM
+            SÃO LÁZARO, A QUAL PERMANECE ATÉ A VOLTA DE CRISTO.
           </p>{' '}
           <br />
-          <p>
-            Somos filiados a Convenção Batista Brasileira e estamos localizados
-            na Av. Duque de Caxias, 565 – Bairro Fragata – Pelotas / RS.
-          </p>
         </div>
         <div className="img">
           <img src={Sobre} alt="" />
